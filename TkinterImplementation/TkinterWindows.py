836ed491366a06b52e0fbde000018cef5e45be9c
Annie Bonav <anniebonav@outlook.com>
2024-03-13 18:22:53 -0600

    Class is working

--- TkinterImplementation/TkinterWindows.py
+++ TkinterImplementation/TkinterWindows.py
@@ -45,7 +45,7 @@ class WindowsPressOnUI(ctk.CTk):
             "currentNumsLabel" : 7,
             "minNumRow" : 8,
             "maxNumRow" : 9,
-            "colorDataRow" : 10,
+            "otherDataRow" : 10,
             "visualizationSize" : 400,
             "initialMin" : 1,
             "initialMax" : 650,
@@ -57,7 +57,7 @@ class WindowsPressOnUI(ctk.CTk):
         self.initializeComPorts()
 
         #
-        # ELEMENTS
+        # ELEMENTS Section
         #
 
         # Title
@@ -68,87 +68,171 @@ class WindowsPressOnUI(ctk.CTk):
         self.greeting = ctk.CTkLabel(self, text = "Hello! This is Press-On, an Application designed to help you visualize the pressure that a user is applying to a ball.", font=("Arial", 16), wraplength=450, pady=10, padx=10, justify="center")
         self.greeting.grid(row = self.uiValues["greetingRow"], column = 0, columnspan = 2, padx = 20)
         
-        #
-        # Setup Canvas
-        #
-        canvas = ctk.CTkCanvas(self, width = self.uiValues["visualizationSize"], height = self.uiValues["visualizationSize"])
-        canvas.grid(row = self.uiValues["canvasRow"], column = 0, padx=40, pady = 20, columnspan = 2)
+        # Canvas
+        self.canvas = ctk.CTkCanvas(self, width = self.uiValues["visualizationSize"], height = self.uiValues["visualizationSize"])
+        self.canvas.grid(row = self.uiValues["canvasRow"], column = 0, padx=40, pady = 20, columnspan = 2)
+        # Visualization
+        self.visualization = self.canvas.create_oval(10, 10, 400, 400, fill="white")
 
         #
-        # COM Port selection
+        # COM Port Section
         #
-        comComboBox = ctk.CTkComboBox(self, values = self.comPorts, font=("Arial", 14), command = lambda : self.comPicker(self,self.comComboBox.get()))
-        comComboBox.grid(row = self.uiValues["comInfoRow"], column = 0)
-        
+
+        # Combo COM Port selection
+        self.comComboBox = ctk.CTkComboBox(self, values = self.comPorts, font=("Arial", 14), command = lambda : self.comPicker(self,self.comComboBox.get()))
+        self.comComboBox.grid(row = self.uiValues["comInfoRow"], column = 0)
+                
         self.currentCom.trace_add("write", self.handleComChange)
-        self.currentCom.set(comComboBox.get())
+        self.currentCom.set(self.comComboBox.get())
         for com in self.comInfo:
             if re.search(r"USB Serial", com):
                 print(f"Found COM Port with 'USB Serial': {com}")
-                comComboBox.set(self.comPorts[self.comInfo.index(com)])
+                self.comComboBox.set(self.comPorts[self.comInfo.index(com)])
                 self.currentCom.set(self.comPorts[self.comInfo.index(com)])
                 break
+        
+        # Refresh COM Port button
+        self.refreshComPortButton = ctk.CTkButton(self, text = "Refresh COM Ports", command = self.refreshComPorts, font=("Arial", 14))
+        self.refreshComPortButton.grid(row = self.uiValues["comInfoRow"], column = 1)
 
-        refreshComPortButton = ctk.CTkButton(self, text = "Refresh COM Ports", command = self.refreshComPorts, font=("Arial", 14))
-        refreshComPortButton.grid(row = self.uiValues["comInfoRow"], column = 1)
-
+        #
+        # Debug Section
         #
         # Debug button
-        #
-
         debugButton = ctk.CTkButton(self, text = "Debug", command = self.toggleDebugging, width = 10, font=("Arial", 14))
         debugButton.grid(row = self.uiValues["debugButtonRow"], column = 0, columnspan = 2, pady = 10)
 
-        #
-        # Setup debugging Frame
-        #
-        debuggingFrame = ctk.CTkFrame(self, width=self.uiValues["screenWidth"]//3*2, height=self.uiValues["screenHeight"]//4)
-        debuggingFrame.grid(row = self.uiValues["debuggingFrameRow"], column = 0, columnspan = 2)
+        # Debugging Frame
+        self.debuggingFrame = ctk.CTkFrame(self, width=self.uiValues["screenWidth"]//3*2, height=self.uiValues["screenHeight"]//4)
+        self.debuggingFrame.grid(row = self.uiValues["debuggingFrameRow"], column = 0, columnspan = 2)
 
         # Debugging section
-        currentRawInputLabel = ctk.CTkLabel(debuggingFrame, text=f"Raw input: -1", font=("Arial", 16))
-        currentRawInputLabel.grid(row = self.uiValues["currentRawInputRow"], column = 0)
+        self.currentRawInputLabel = ctk.CTkLabel(self.debuggingFrame, text=f"Raw input: -1", font=("Arial", 16))
+        self.currentRawInputLabel.grid(row = self.uiValues["currentRawInputRow"], column = 0)
 
         #
-        # Min Max labels and input
+        # Min Max Section
         #
-        minNumLabel = ctk.CTkLabel(debuggingFrame, text = f"Current Min: {self.uiValues['initialMin']}", font=("Arial", 16))
-        minNumLabel.grid(row = self.uiValues["currentNumsLabel"], column = 0)
-        maxNumLabel = ctk.CTkLabel(debuggingFrame, text = f"Current Max: {self.uiValues['initialMax']}", font=("Arial", 16))
-        maxNumLabel.grid(row = self.uiValues["currentNumsLabel"], column = 1, pady=10)
 
-        minNumLabelTitle = ctk.CTkLabel(debuggingFrame, text="Min: ", font=("Arial", 16))
-        minNumLabelTitle.grid(row = self.uiValues["minNumRow"], column = 0)
-
+        # Current MinNum Label
+        self.currentMinNumLabel = ctk.CTkLabel(self.debuggingFrame, text = f"Current Min: {self.uiValues['initialMin']}", font=("Arial", 16))
+        self.currentMinNumLabel.grid(row = self.uiValues["currentNumsLabel"], column = 0)
+        # Min num label
+        self.minNumLabel = ctk.CTkLabel(self.debuggingFrame, text="Min: ", font=("Arial", 16))
+        self.minNumLabel.grid(row = self.uiValues["minNumRow"], column = 0)
+        # Min num input	variable
         minVar = ctk.IntVar()
         minVar.set(self.uiValues["initialMin"])
-        minNumInput = ctk.CTkEntry(debuggingFrame, textvariable = minVar, justify="right")
+        # Min num input
+        minNumInput = ctk.CTkEntry(self.debuggingFrame, textvariable = minVar, justify="right")
         minNumInput.grid(row = self.uiValues["minNumRow"], column = 1)
 
-        maxNumLabelTitle = ctk.CTkLabel(debuggingFrame, text = "Max: ", font=("Arial", 16))
-        maxNumLabelTitle.grid(row = self.uiValues["maxNumRow"], column = 0, pady=10)
-
+        # Current MaxNum Label
+        self.currentMaxNumLabel = ctk.CTkLabel(self.debuggingFrame, text = f"Current Max: {self.uiValues['initialMax']}", font=("Arial", 16))
+        self.currentMaxNumLabel.grid(row = self.uiValues["currentNumsLabel"], column = 1, pady=10)
+        # Max num label
+        self.maxNumLabelTitle = ctk.CTkLabel(self.debuggingFrame, text = "Max: ", font=("Arial", 16))
+        self.maxNumLabelTitle.grid(row = self.uiValues["maxNumRow"], column = 0, pady=10)
+        # Max num input variable
         maxVar = ctk.IntVar()
         maxVar.set(self.uiValues["initialMax"])
-        maxNumInput = ctk.CTkEntry(debuggingFrame, textvariable = maxVar, justify="right")
+        # Max num input
+        maxNumInput = ctk.CTkEntry(self.debuggingFrame, textvariable = maxVar, justify="right")
         maxNumInput.grid(row = self.uiValues["maxNumRow"], column = 1)
 
-        normalizedInputLabel = ctk.CTkLabel(debuggingFrame, text=f"Normalized input: -1", width = 150)
-        normalizedInputLabel.grid(row = self.uiValues["colorDataRow"], column = 0)
+        #
+        # Other data section
+        #
+        # Normalized data label
+        self.normalizedInputLabel = ctk.CTkLabel(self.debuggingFrame, text=f"Normalized input: -1", width = 150)
+        self.normalizedInputLabel.grid(row = self.uiValues["otherDataRow"], column = 0)
+        # RGB color label
+        self.rgbColorLabel = ctk.CTkLabel(self.debuggingFrame, text = f"RGB: -1, -1, -1", width = 150)
+        self.rgbColorLabel.grid(row = self.uiValues["otherDataRow"], column = 1)
+        # Hex color label
+        self.hexColorLabel = ctk.CTkLabel(self.debuggingFrame, text = f"Hex: #F00000", width = 150)
+        self.hexColorLabel.grid(row = self.uiValues["otherDataRow"], column = 2)
 
-        rgbColorLabel = ctk.CTkLabel(debuggingFrame, text = f"RGB: -1, -1, -1", width = 150)
-        rgbColorLabel.grid(row = self.uiValues["colorDataRow"], column = 1)
-
-        hexColorLabel = ctk.CTkLabel(debuggingFrame, text = f"Hex: #F00000", width = 150)
-        hexColorLabel.grid(row = self.uiValues["colorDataRow"], column = 2)
-
-        visualization = canvas.create_oval(10, 10, 400, 400, fill="white")
-
+        # Handling starting window stuff
         self.update_idletasks()
         # self.withdraw()
         self.geometry(f"+{(self.winfo_screenwidth() - self.winfo_reqwidth()) // 2}+{(self.winfo_screenheight() - self.winfo_reqheight()) // 2}")
         # self.deiconify()
+        self.debuggingFrame.grid_forget() # Starts with forgetting the debugging frame so it doesnt appear
+
+        elementsDictionary = {
+            "currentMinNumLabel": self.currentMinNumLabel,
+            "currentMaxNumLabel": self.currentMaxNumLabel,
+            "canvas": self.canvas,
+            "visualization": self.visualization,
+            "rgbColorLabel": self.rgbColorLabel,
+            "hexColorLabel" : self.hexColorLabel,
+            "currentRawInputLabel" : self.currentRawInputLabel,
+            "normalizedInputLabel" : self.normalizedInputLabel,
+            "currentCom" : self.currentCom
+        }
+
+        # Initializes the serial thread with the elements dictionary (so the thread can update the elements) and the com port (so the thread can use it to begin the listening)
+        self.serialThread.initialize(elementsDictionary, self.comComboBox.get())
+        self.serialThread.start()
+
+        self.minNumSave = ctk.CTkButton(self.debuggingFrame, text="Save", command = self.updateMin)
+        self.minNumSave.grid(row = self.uiValues["minNumRow"], column = 2)
+
+        maxNumSave = ctk.CTkButton(self.debuggingFrame, text="Save", command = self.updateMax)
+        maxNumSave.grid(row = self.uiValues["maxNumRow"], column = 2)
+
+        self.protocol("WM_DELETE_WINDOW", self.onClosing)
         self.mainloop()
+
+    def onClosing(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.destroy()
+
+    def popup(errorType) :
+        messagebox.showerror("Error updating the numbers",errorType.input)  
+
+    def updateMin(self):
+        min = self.minVar.get()
+        max = self.maxVar.get()
+        print("GOT MIN")
+        try:
+            min = int(min)
+        except:
+            self.popup(ErrorType.INVALID_MIN_NUMBER)
+            return
+        
+        if min <= 0:
+            self.popup(ErrorType.MIN_NUM_SMALLER_THAN_1)
+            return
+        
+        elif min >= max:
+            self.popup(ErrorType.MIN_NUM_BIGGER_THAN_MAX_NUM)
+            return
+        
+        print(f"Updating Min: {min}")
+        self.serialThread.updateMin(min)
+
+    def updateMax(self):
+        min = self.minVar.get()
+        max = self.maxVar.get()
+        print("GOT MAX")
+        try:
+            max = int(max)
+        except:
+            self.popup(ErrorType.INVALID_MAX_NUMBER)
+            return
+        
+        if max <= min:
+            self.popup(ErrorType.MAX_NUM_SMALLER_THAN_MAX_NUM)
+            return
+        
+        if max > 1023:
+            self.popup(ErrorType.MAX_NUM_BIGGER_THAN_1023)
+            return
+
+        print(f"Updating Max: {max}")
+        self.serialThread.updateMax(max)
 
     #
     # METHODS
@@ -220,86 +304,3 @@ class WindowsPressOnUI(ctk.CTk):
 
 if __name__ == "__main__":
     app = WindowsPressOnUI()
-    
-
-
-def RunUI():
-        
-    
-    
-
-    debuggingFrame.grid_forget() # Starts with forgetting the debugging frame so it doesnt appear
-    elementsDictionary = {
-        "minNumLabel": minNumLabel,
-        "maxNumLabel": maxNumLabel,
-        "canvas": canvas,
-        "visualization": visualization,
-        "rgbColorLabel": rgbColorLabel,
-        "hexColorLabel" : hexColorLabel,
-        "currentRawInputLabel" : currentRawInputLabel,
-        "normalizedInputLabel" : normalizedInputLabel,
-        "currentCom" : currentCom
-    }
-
-    serialThread.initialize(elementsDictionary, comComboBox.get())
-    serialThread.start()
-
-    def popup(errorType) :
-        messagebox.showerror("Error updating the numbers",errorType.input)  
-
-    def updateMin():
-        min = minVar.get()
-        max = maxVar.get()
-        print("GOT MIN")
-        try:
-            min = int(min)
-        except:
-            popup(ErrorType.INVALID_MIN_NUMBER)
-            return
-        
-        if min <= 0:
-            popup(ErrorType.MIN_NUM_SMALLER_THAN_1)
-            return
-        
-        elif min >= max:
-            popup(ErrorType.MIN_NUM_BIGGER_THAN_MAX_NUM)
-            return
-        
-        print(f"Updating Min: {min}")
-        serialThread.updateMin(min)
-
-    def updateMax():
-        min = minVar.get()
-        max = maxVar.get()
-        print("GOT MAX")
-        try:
-            max = int(max)
-        except:
-            popup(ErrorType.INVALID_MAX_NUMBER)
-            return
-        
-        if max <= min:
-            popup(ErrorType.MAX_NUM_SMALLER_THAN_MAX_NUM)
-            return
-        
-        if max > 1023:
-            popup(ErrorType.MAX_NUM_BIGGER_THAN_1023)
-            return
-
-        print(f"Updating Max: {max}")
-        serialThread.updateMax(max)
-
-    minNumSave = ctk.CTkButton(debuggingFrame, text="Save", command = updateMin)
-    minNumSave.grid(row = minNumRow, column = 2)
-
-    maxNumSave = ctk.CTkButton(debuggingFrame, text="Save", command = updateMax)
-    maxNumSave.grid(row = maxNumRow, column = 2)
-
-
-    def on_closing():
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            root.destroy()
-
-    root.protocol("WM_DELETE_WINDOW", on_closing)
-
-    root.mainloop()
